03bf2d9e1ced955804ff82b8c2cce12469daa294
TNoVT <135317262+TNoVT@users.noreply.github.com>
2023-06-22 16:45:27 +0700

    Update adb.js

--- script/adb.js
+++ script/adb.js
@@ -2,94 +2,31 @@ var antiAdb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=
 var antiAdbK = antiAdb.split("");
 var adbChek = document.querySelectorAll(".wc-credit").length;
 var adbChekA = document.querySelector(".wc-credit");
-if (adbChek != 0) {
- setTimeout(function() {
-   var ta = document.querySelectorAll(".adsbygoogle")[0];
-   var style = window.getComputedStyle(ta, null);
-   if (style.display == "none" || style.visibility == "hidden" || style.opacity == "0" || style.overflow == "hidden") {
-     akadb();
-   }
-   if (!document.querySelectorAll("ins")[0].classList.contains("adsbygoogle")) {
-     akadb();
-   }
- }, 1000);
- var caPub;
- var add = document.createElement("script");
- var crs = document.createAttribute("crossorigin");
- crs.value = "anonymous";
- add.setAttributeNode(crs);
- add.async = true;
- add.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + caPub;
- add.onerror = function() {
-   akadb();
- };
- document.head.appendChild(add);
- setTimeout(function() {
-   if (document.getElementById("aswift_0_host") == null && document.getElementById("aswift_1_host") == null && document.getElementById("aswift_2_host") == null && document.getElementById("aswift_3_host") == null && document.getElementById("aswift_4_host") == null && document.getElementById("aswift_5_host") == null) {
-     akadb();
-   }
-   if (document.querySelectorAll("ins.adsbygoogle").length <= 0 && document.querySelector("ins.adsbygoogle").offsetHeight === 0) {
-     akadb();
-   }
- }, 5000);
- !function(frame, toArray) {
-   var getUtf8LengthForString = function() {
-     var y$$ = true;
-     return function(body, fmt) {
-       var voronoi = y$$ ? function() {
-         if (fmt) {
-           var code = fmt.apply(body, arguments);
-           return fmt = null, code;
-         }
-       } : function() {
+var lazyadsense = false;
+ window.addEventListener("scroll", function() {
+   if (0 != document.documentElement.scrollTop && false === lazyadsense || 0 != document.body.scrollTop && false === lazyadsense) {
+     !function() {
+       var link = document.createElement("script");
+       link.setAttribute("crossorigin", "anonymous");
+       link.async = true;
+       link.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + caPub;
+       link.onerror = function() {
+         akadb();
        };
-       return y$$ = false, voronoi;
-     };
-   }();
-   var length = getUtf8LengthForString(this, function() {
-     return length.toString().search("(((.+)+)+)+$").toString().constructor(length).search("(((.+)+)+)+$");
-   });
-   length();
-   var obj = function() {
-     var y$$ = true;
-     return function(body, fmt) {
-       var voronoi = y$$ ? function() {
-         if (fmt) {
-           var code = fmt.apply(body, arguments);
-           return fmt = null, code;
+       var wafCss = document.getElementsByTagName("script")[0];
+       wafCss.parentNode.insertBefore(link, wafCss);
+       window.setTimeout(function() {
+         if (null == document.getElementById("aswift_0_host") && null == document.getElementById("aswift_1_host") && null == document.getElementById("aswift_2_host") && null == document.getElementById("aswift_3_host") && null == document.getElementById("aswift_4_host") && null == document.getElementById("aswift_5_host")) {
+           akadb();
          }
-       } : function() {
-       };
-       return y$$ = false, voronoi;
-     };
-   }();
-   var x = obj(this, function() {
-     var getAlignItem = function model() {
-       var object;
-       try {
-         object = function() {
-           return function() {
-           }.constructor("return this")();
-         }();
-       } catch (_0x4b1f64) {
-         object = window;
-       }
-       return object;
-     };
-     var alignContentAlignItem = getAlignItem();
-     var deviceStates = alignContentAlignItem.console = alignContentAlignItem.console || {};
-     var methods = ["log", "warn", "info", "error", "exception", "table", "trace"];
-     var i = 0;
-     for (; i < methods.length; i++) {
-       var params = obj.constructor.prototype.bind(obj);
-       var key = methods[i];
-       var provider = deviceStates[key] || params;
-       params.__proto__ = obj.bind(obj);
-       params.toString = provider.toString.bind(provider);
-       deviceStates[key] = params;
-     }
-   });
-   x();
+         if (document.querySelectorAll("ins.adsbygoogle").length <= 0 && 0 === document.querySelector("ins.adsbygoogle").offsetHeight) {
+           akadb();
+         }
+       }, 5E3);
+     }();
+     lazyadsense = true;
+   }, true);
+ (function(frame, toArray) {
    if ("function" == typeof define && define.amd) {
      define(toArray);
    } else {
@@ -99,25 +36,22 @@ if (adbChek != 0) {
        frame.adblock = toArray();
      }
    }
- }(undefined, function() {
+ })(undefined, function() {
    var PWWWService = {};
-   var check_by_re = function t() {
-     var bezierPreviewContainer = document.createElement("div");
-     return bezierPreviewContainer.classList.add("adsbygoogle", "ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"), bezierPreviewContainer.id = "adcontent", document.body.appendChild(bezierPreviewContainer), bezierPreviewContainer;
-   };
    return PWWWService.check = function(cb, threshold) {
-     var caret_insertion = check_by_re();
+     var caret_insertion = function() {
+       var bezierPreviewContainer = document.createElement("div");
+       return bezierPreviewContainer.classList.add("adsbygoogle", "ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"), bezierPreviewContainer.id = "adcontent", document.body.appendChild(bezierPreviewContainer), bezierPreviewContainer;
+     }();
      setTimeout(function() {
        var iconCtx = 0 === caret_insertion.offsetLeft && 0 === caret_insertion.offsetTop;
        cb(iconCtx);
-     }, threshold || 1000);
+     }, threshold || 1E3);
    }, PWWWService;
  });
  adblock.check(function(canCreateDiscussions) {
    if (canCreateDiscussions) {
      akadb();
    }
- }, 1000);
-} else {
- window.location.href = antiAdbK[7] + antiAdbK[19] + antiAdbK[19] + antiAdbK[15] + "://" + antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + "." + antiAdbK[22] + antiAdbK[4] + antiAdbK[13] + antiAdbK[3] + antiAdbK[24] + antiAdbK[2] + antiAdbK[14] + antiAdbK[3] + antiAdbK[4] + "." + antiAdbK[2] + antiAdbK[14] + antiAdbK[12] + "/" + antiAdbK[15] + "/" + antiAdbK[2] + antiAdbK[17] + antiAdbK[4] + antiAdbK[3] + antiAdbK[8] + antiAdbK[19] + "." + antiAdbK[7] + antiAdbK[19] + antiAdbK[12] + antiAdbK[11];
+ }, 1E3);
 };
