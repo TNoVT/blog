eff05d730a2b4b3923e0d0a6e53b2d1b963f0afb
TNoVT <135317262+TNoVT@users.noreply.github.com>
2023-06-22 16:25:41 +0700

    Update adb.js

--- script/adb.js
+++ script/adb.js
@@ -1,9 +1,8 @@
-var antiAdb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=.:";
-var antiAdbK = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=.:".split("");
-var a = document.querySelector('meta[property="og:url"]').getAttribute("content").split("/")[2];
-var b = antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + antiAdbK[65] + antiAdbK[2] + antiAdbK[7] + antiAdbK[14] + antiAdbK[8] + antiAdbK[15] + antiAdbK[0] + antiAdbK[13] + antiAdbK[22] + antiAdbK[4] + antiAdbK[13] + antiAdbK[3] + antiAdbK[24] + antiAdbK[65] + antiAdbK[2] + antiAdbK[14] + antiAdbK[12];
-var c = antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + antiAdbK[65] + antiAdbK[12] + antiAdbK[0] + antiAdbK[18] + antiAdbK[14] + antiAdbK[3] + antiAdbK[4] + antiAdbK[4] + antiAdbK[65] + antiAdbK[2] + antiAdbK[14] + antiAdbK[12];
-if (a === b || a === c) {
+var antiAdb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+var antiAdbK = antiAdb.split("");
+var adbChek = document.querySelectorAll(".wc-credit").length;
+var adbChekA = document.querySelector(".wc-credit");
+if (){
  setTimeout(function() {
    var ta = document.querySelectorAll(".adsbygoogle")[0];
    var style = window.getComputedStyle(ta, null);
@@ -24,9 +23,8 @@ if (a === b || a === c) {
  add.onerror = function() {
    akadb();
  };
- var sc = document.getElementsByTagName("head")[0];
- sc.parentNode.insertBefore(add, sc);
- window.setTimeout(function() {
+ document.head.appendChild(add);
+ setTimeout(function() {
    if (document.getElementById("aswift_0_host") == null && document.getElementById("aswift_1_host") == null && document.getElementById("aswift_2_host") == null && document.getElementById("aswift_3_host") == null && document.getElementById("aswift_4_host") == null && document.getElementById("aswift_5_host") == null) {
      akadb();
    }
@@ -36,14 +34,16 @@ if (a === b || a === c) {
  }, 5000);
  !function(frame, toArray) {
    var getUtf8LengthForString = function() {
+     var y$$ = true;
      return function(body, fmt) {
-       var _0x175691 = function CanSimpleDocument() {
+       var voronoi = y$$ ? function() {
          if (fmt) {
            var code = fmt.apply(body, arguments);
            return fmt = null, code;
          }
+       } : function() {
        };
-       return _0x175691;
+       return y$$ = false, voronoi;
      };
    }();
    var length = getUtf8LengthForString(this, function() {
@@ -51,28 +51,33 @@ if (a === b || a === c) {
    });
    length();
    var obj = function() {
+     var y$$ = true;
      return function(body, fmt) {
-       var _0x1a857b = function CanSimpleDocument() {
+       var voronoi = y$$ ? function() {
          if (fmt) {
            var code = fmt.apply(body, arguments);
            return fmt = null, code;
          }
+       } : function() {
        };
-       return _0x1a857b;
+       return y$$ = false, voronoi;
      };
    }();
    var x = obj(this, function() {
-     var lastrayafteradjust;
-     try {
-       var vec3 = function loadFromPath() {
-         return function() {
-         }.constructor("return this")();
-       };
-       lastrayafteradjust = vec3();
-     } catch (_0x12cf68) {
-       lastrayafteradjust = window;
-     }
-     var deviceStates = window.console = window.console || {};
+     var getAlignItem = function model() {
+       var object;
+       try {
+         object = function() {
+           return function() {
+           }.constructor("return this")();
+         }();
+       } catch (_0x4b1f64) {
+         object = window;
+       }
+       return object;
+     };
+     var alignContentAlignItem = getAlignItem();
+     var deviceStates = alignContentAlignItem.console = alignContentAlignItem.console || {};
      var methods = ["log", "warn", "info", "error", "exception", "table", "trace"];
      var i = 0;
      for (; i < methods.length; i++) {
@@ -114,6 +119,5 @@ if (a === b || a === c) {
    }
  }, 1000);
 } else {
- window.location.href = antiAdbK[7] + antiAdbK[19] + antiAdbK[19] + antiAdbK[15] + antiAdbK[18] + antiAdbK[66] + antiAdbK[63] + antiAdbK[63] + antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + antiAdbK[65] + antiAdbK[22] + antiAdbK[4] + antiAdbK[13] + antiAdbK[3] + antiAdbK[24] + antiAdbK[2] + antiAdbK[14] + antiAdbK[3] + antiAdbK[4] + antiAdbK[65] + antiAdbK[2] + antiAdbK[14] + antiAdbK[12] + antiAdbK[63] + antiAdbK[15] + antiAdbK[63] + antiAdbK[2] + antiAdbK[17] + antiAdbK[4] + antiAdbK[3] + antiAdbK[8] +
- antiAdbK[19] + antiAdbK[65] + antiAdbK[7] + antiAdbK[19] + antiAdbK[12] + antiAdbK[11];
+ window.location.href = antiAdbK[7] + antiAdbK[19] + antiAdbK[19] + antiAdbK[15] + "://" + antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + "." + antiAdbK[22] + antiAdbK[4] + antiAdbK[13] + antiAdbK[3] + antiAdbK[24] + antiAdbK[2] + antiAdbK[14] + antiAdbK[3] + antiAdbK[4] + "." + antiAdbK[2] + antiAdbK[14] + antiAdbK[12] + "/" + antiAdbK[15] + "/" + antiAdbK[2] + antiAdbK[17] + antiAdbK[4] + antiAdbK[3] + antiAdbK[8] + antiAdbK[19] + "." + antiAdbK[7] + antiAdbK[19] + antiAdbK[12] + antiAdbK[11];
 };
