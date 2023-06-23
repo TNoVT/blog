dc94586c168811c63869e4a949a7281f4e982ef1
TNoVT <135317262+TNoVT@users.noreply.github.com>
2023-06-23 08:41:13 +0700

    initial commit

--- script/adb.js
+++ script/adb.js
@@ -1,57 +1,100 @@
-var antiAdb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
-var antiAdbK = antiAdb.split("");
-var adbChek = document.querySelectorAll(".wc-credit").length;
-var adbChekA = document.querySelector(".wc-credit");
-var lazyadsense = false;
- window.addEventListener("scroll", function() {
-   if (0 != document.documentElement.scrollTop && false === lazyadsense || 0 != document.body.scrollTop && false === lazyadsense) {
-     !function() {
-       var link = document.createElement("script");
-       link.setAttribute("crossorigin", "anonymous");
-       link.async = true;
-       link.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + caPub;
-       link.onerror = function() {
-         akadb();
-       };
-       var wafCss = document.getElementsByTagName("script")[0];
-       wafCss.parentNode.insertBefore(link, wafCss);
-       window.setTimeout(function() {
-         if (null == document.getElementById("aswift_0_host") && null == document.getElementById("aswift_1_host") && null == document.getElementById("aswift_2_host") && null == document.getElementById("aswift_3_host") && null == document.getElementById("aswift_4_host") && null == document.getElementById("aswift_5_host")) {
-           akadb();
-         }
-         if (document.querySelectorAll("ins.adsbygoogle").length <= 0 && 0 === document.querySelector("ins.adsbygoogle").offsetHeight) {
-           akadb();
-         }
-       }, 5E3);
-     }();
-     lazyadsense = true;
-   }, true);
- (function(frame, toArray) {
-   if ("function" == typeof define && define.amd) {
-     define(toArray);
-   } else {
-     if ("object" == (typeof exports === "undefined" ? "undefined" : _typeof(exports))) {
-       module.exports = toArray;
-     } else {
-       frame.adblock = toArray();
-     }
-   }
- })(undefined, function() {
-   var PWWWService = {};
-   return PWWWService.check = function(cb, threshold) {
-     var caret_insertion = function() {
-       var bezierPreviewContainer = document.createElement("div");
-       return bezierPreviewContainer.classList.add("adsbygoogle", "ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"), bezierPreviewContainer.id = "adcontent", document.body.appendChild(bezierPreviewContainer), bezierPreviewContainer;
-     }();
-     setTimeout(function() {
-       var iconCtx = 0 === caret_insertion.offsetLeft && 0 === caret_insertion.offsetTop;
-       cb(iconCtx);
-     }, threshold || 1E3);
-   }, PWWWService;
- });
- adblock.check(function(canCreateDiscussions) {
-   if (canCreateDiscussions) {
-     akadb();
-   }
- }, 1E3);
-};
+var antiAdb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=.:',
+  antiAdbK = antiAdb.split('');
+var a = document.querySelector('meta[property="og:url"]').getAttribute('content').split('/')[2];
+var b = antiAdbK[13] + antiAdbK[14] + antiAdbK[21] + antiAdbK[4] + antiAdbK[11] + antiAdbK[67] + antiAdbK[19] + antiAdbK[19] + antiAdbK[65] + antiAdbK[1] + antiAdbK[11] + antiAdbK[14] + antiAdbK[6] + antiAdbK[18] + antiAdbK[15] + antiAdbK[14] + antiAdbK[19] + antiAdbK[65] + antiAdbK[2] + antiAdbK[14] + antiAdbK[12]; 
+if (a === b) {
+  setTimeout(function () {
+    var _0x124e3f = document.querySelectorAll('.adsbygoogle')[0];
+    var _0xd51d05 = window.getComputedStyle(_0x124e3f, null);
+    if (_0xd51d05.display == 'none' || _0xd51d05.visibility == 'hidden' || _0xd51d05.opacity == '0' || _0xd51d05.overflow == 'hidden') {
+      akadb();
+    }
+    ;
+    if (!document.querySelectorAll('ins')[0].classList.contains('adsbygoogle')) {
+      akadb();
+    }
+  }, 1000);
+  var caPub,
+    add = document.createElement('script'),
+    crs = document.createAttribute('crossorigin');
+  crs.value = 'anonymous', add.setAttributeNode(crs), add.async = true, add.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + caPub, add.onerror = function () {
+    akadb();
+  };
+  var sc = document.getElementsByTagName('head')[0];
+  sc.parentNode.insertBefore(add, sc), window.setTimeout(function () {
+    document.getElementById('aswift_0_host') == null && document.getElementById('aswift_1_host') == null && document.getElementById('aswift_2_host') == null && document.getElementById('aswift_3_host') == null && document.getElementById('aswift_4_host') == null && document.getElementById('aswift_5_host') == null && akadb();
+    ;
+    document.querySelectorAll('ins.adsbygoogle').length <= 0 && document.querySelector('ins.adsbygoogle').offsetHeight === 0 && akadb();
+    ;
+  }, 5000), !function (_0x364492, _0x16788a) {
+    var _0x3b1023 = function () {
+        var _0x1a7ba3 = true;
+        return function (_0x2d9e37, _0x4b4a8b) {
+          var _0x175691 = _0x1a7ba3 ? function () {
+            if (_0x4b4a8b) {
+              var _0x5690f4 = _0x4b4a8b.apply(_0x2d9e37, arguments);
+              return _0x4b4a8b = null, _0x5690f4;
+            }
+          } : function () {};
+          return _0x1a7ba3 = false, _0x175691;
+        };
+      }(),
+      _0x4d1a03 = _0x3b1023(this, function () {
+        return _0x4d1a03.toString().search('(((.+)+)+)+$').toString().constructor(_0x4d1a03).search('(((.+)+)+)+$');
+      });
+    _0x4d1a03();
+    var _0x552307 = function () {
+      var _0x4a3ef6 = true;
+      return function (_0x29d449, _0x1fc739) {
+        var _0x1a857b = _0x4a3ef6 ? function () {
+          if (_0x1fc739) {
+            var _0x2788e5 = _0x1fc739.apply(_0x29d449, arguments);
+            return _0x1fc739 = null, _0x2788e5;
+          }
+        } : function () {};
+        return _0x4a3ef6 = false, _0x1a857b;
+      };
+    }();
+    var _0x16ec71 = _0x552307(this, function () {
+      var _0x4fd6b8;
+      try {
+        var _0xfd764b = function () {
+          return function () {}.constructor("return this")();
+        };
+        _0x4fd6b8 = _0xfd764b();
+      } catch (_0x12cf68) {
+        _0x4fd6b8 = window;
+      }
+      var _0x47c417 = _0x4fd6b8.console = _0x4fd6b8.console || {},
+        _0x14cad1 = ['log', 'warn', 'info', 'error', 'exception', 'table', 'trace'];
+      for (var _0x4f4986 = 0; _0x4f4986 < _0x14cad1.length; _0x4f4986++) {
+        var _0x1a612 = _0x552307.constructor.prototype.bind(_0x552307);
+        var _0x8df98c = _0x14cad1[_0x4f4986];
+        var _0x555027 = _0x47c417[_0x8df98c] || _0x1a612;
+        _0x1a612.__proto__ = _0x552307.bind(_0x552307);
+        _0x1a612.toString = _0x555027.toString.bind(_0x555027);
+        _0x47c417[_0x8df98c] = _0x1a612;
+      }
+    });
+    _0x16ec71(), 'function' == typeof define && define.amd ? define(_0x16788a) : 'object' == typeof exports ? module.exports = _0x16788a : _0x364492.adblock = _0x16788a();
+  }(this, function () {
+    'use strict';
+
+    var _0x31ae14 = {},
+      _0xc956f0 = function () {
+        var _0x265a5f = document.createElement('div');
+        return _0x265a5f.classList.add('adsbygoogle', 'ad', 'ads', 'adsbox', 'doubleclick', 'ad-placement', 'ad-placeholder', 'adbadge', 'BannerAd'), (_0x265a5f.id = 'adcontent', document.body.appendChild(_0x265a5f), _0x265a5f);
+      };
+    return _0x31ae14.check = function (_0x4b328f, _0x3d68b1) {
+      var _0xc8519b = _0xc956f0();
+      setTimeout(function () {
+        var _0x286bb8 = 0 === _0xc8519b.offsetLeft && 0 === _0xc8519b.offsetTop;
+        _0x4b328f(_0x286bb8);
+      }, _0x3d68b1 || 1000);
+    }, _0x31ae14;
+  }), adblock.check(function (_0x13511e) {
+    if (_0x13511e) {
+      akadb();
+    }
+  }, 1000);
+} else window.location.href = antiAdbK[7] + antiAdbK[19] + antiAdbK[19] + antiAdbK[15] + antiAdbK[18] + antiAdbK[66] + antiAdbK[63] + antiAdbK[63] + antiAdbK[22] + antiAdbK[22] + antiAdbK[22] + antiAdbK[65] + antiAdbK[22] + antiAdbK[4] + antiAdbK[13] + antiAdbK[3] + antiAdbK[24] + antiAdbK[2] + antiAdbK[14] + antiAdbK[3] + antiAdbK[4] + antiAdbK[65] + antiAdbK[2] + antiAdbK[14] + antiAdbK[12] + antiAdbK[63] + antiAdbK[15] + antiAdbK[63] + antiAdbK[2] + antiAdbK[17] + antiAdbK[4] + antiAdbK[3] + antiAdbK[8] + antiAdbK[19] + antiAdbK[65] + antiAdbK[7] + antiAdbK[19] + antiAdbK[12] + antiAdbK[11];
